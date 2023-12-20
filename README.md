30edd52f8bf1d935a2fc8a00328e34c7c8d6d8fa
John M <entity.john@gmail.com>
2023-12-20 10:46:39 -0600

    using default server multiplexer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # golang-http-server
+https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", getRoot)
+	// mux.HandleFunc("/hello", getHello)
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+	// err := http.ListenAndServe(":3333", mux)
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP\n")
+}
